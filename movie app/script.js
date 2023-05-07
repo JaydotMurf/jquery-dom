c1eedae529794a60425d415c162da17511c8c686
jor-el <jerraill.d.murphy@gmail.com>
2023-05-07 15:13:40 +0200

    working version, TODO: refactor

--- movie app/script.js
+++ movie app/script.js
@@ -1,9 +1,31 @@
 $('document').ready(() => {
   $('#movie-form').on('submit', (e) => {
-    console.log('Form submitted');
     e.preventDefault();
     const movieTitle = $('#title').val();
     const movieRating = $('#rating').val();
-    $('#movie-list').append(`<li>${movieTitle} | ${movieRating}</li>`);
+    $('#movie-list').append(
+      `<li><span class="movie-title">${movieTitle}</span>: ${movieRating}</li>`
+    );
+  });
+
+  $('#sort-btn').on('click', () => {
+    const movieList = $('#movie-list').children().toArray();
+
+    const sortedList = movieList.sort((a, b) => {
+      const titleA = $(a).find('.movie-title').text().toLowerCase();
+      const titleB = $(b).find('.movie-title').text().toLowerCase();
+      if (titleA < titleB) {
+        return -1;
+      } else if (titleA > titleB) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    // Append the sorted list back to the movie list
+    sortedList.forEach((movie) => {
+      $('#movie-list').append(movie);
+    });
   });
 });
